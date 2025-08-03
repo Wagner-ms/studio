8e07e34752ba141ad86f36f414cb52cfe1ce4f94
Wagner Sousa <wagnersousa915@gmail.com>
2025-08-03 20:27:55 +0000

    o problema com o layout ainda não foi resolvido

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,12 +9,10 @@ export default function MainLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40 sm:flex-row">
         <MainNav />
-        <main className="flex-1 p-4 sm:p-6">
+        <main className="flex-1 p-4 sm:p-6 bg-muted/40">
           {children}
         </main>
-      </div>
     </SidebarProvider>
   );
 }
