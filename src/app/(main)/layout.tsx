f9f71ca6fe2a2851f0dba45cfd223a9f634a19c6
Wagner Sousa <wagnersousa915@gmail.com>
2025-08-03 20:07:38 +0000

    consegue ver essa imagen ela é do nosso projeto

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40 sm:flex-row">
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <MainNav />
         <main className="flex flex-1 flex-col gap-4 p-4 sm:gap-8 sm:p-6 sm:py-8">
           {children}
