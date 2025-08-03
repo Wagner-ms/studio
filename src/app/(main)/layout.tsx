953c3d952eeb41ca9af398070aa7291e47e61d2c
Wagner Sousa <wagnersousa915@gmail.com>
2025-08-03 20:13:58 +0000

    o problema ainda pesiste

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,13 +9,11 @@ export default function MainLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col sm:flex-row bg-muted/40">
+      <div className="flex min-h-screen w-full flex-col bg-muted/40 sm:flex-row">
         <MainNav />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 w-full">
-            <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-              {children}
-            </main>
-        </div>
+        <main className="flex-1 p-4 sm:p-6">
+          {children}
+        </main>
       </div>
     </SidebarProvider>
   );
